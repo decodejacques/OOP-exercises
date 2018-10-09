d86d09d890142b3abe4e4a66e4f28966264b65a9
jacques <jacqueslenormand@gmail.com>
2018-10-09 16:21:43 -0400

    first commit

---
+++ 0/problem.js
@@ -0,0 +1,19 @@
+class Dog {
+    // Dog has a constructor with three arguments (in this order): age, name and breed
+    // Dog has three attributes: age, name and breed
+    // Dog has a method bark, which returns a string    
+}
+
+let d = new Dog(3, "good boy", "schnitzel");
+if(d.name !=="good boy") {
+    throw new Error("Incorrect name")
+}
+if(d.age !== 3) {
+    throw new Error("Incorrect age")
+};
+if(d.breed !== "schnitzel") {
+    throw new Error ("Incorred breed")
+};
+if(typeof d.bark !== "string") {
+    throw new Error ("bark method return type incorrect")
+}
